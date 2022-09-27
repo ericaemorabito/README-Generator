ac86cdd0f1f8b633c7a59b120cf9623f7fb58bae
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-09-27 10:21:13 -0500

    All prompts created and working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown'); //imported utils file
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  'Write a description of your project.',
+  'What are the steps required to install your project?',
+  'What license did you use?'
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const runCode = function init() { 
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Write a description of your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Write the steps required to install your project.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Write the instructions and examples of use.'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Write the contribution guidelines.'
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Write the test instructions.'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose your license.',
+      choices: ['license 1', 'license2'
+    ],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?'
+    }
+  ])
+  .then((response) =>
+   console.log('done'));
+}
+runCode();
+// Function call to initialize app();
