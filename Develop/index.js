4375c0b09779afa1775b4b5845172276fb12a5ff
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-09-27 10:47:08 -0500

    generateMD completed

--- Develop/index.js
+++ Develop/index.js
@@ -4,69 +4,73 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown'); //imported utils file
 
 // TODO: Create an array of questions for user input
-const questions = [
-  'Write a description of your project.',
-  'What are the steps required to install your project?',
-  'What license did you use?'
-];
+// const questions = [
+//   'Write a description of your project.',
+//   'What are the steps required to install your project?',
+//   'What license did you use?'
+// ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+const createREADME = function() {
+  fs.writeFile('README.md', generateMarkdown(data))
+};
 
-// TODO: Create a function to initialize app
-const runCode = function init() { 
+// Function to initialize app
+const runCode = function init() {
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Write a description of your project.'
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Write the steps required to install your project.'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Write the instructions and examples of use.'
-    },
-    {
-      type: 'input',
-      name: 'contribution',
-      message: 'Write the contribution guidelines.'
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: 'Write the test instructions.'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose your license.',
-      choices: ['license 1', 'license2'
-    ],
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is your github username?'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email?'
-    }
-  ])
-  .then((response) =>
-   console.log('done'));
-}
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project.'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Write the steps required to install your project.'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Write the instructions and examples of use.'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Write the contribution guidelines.'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Write the test instructions.'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license.',
+        choices: ['license 1', 'license2'
+        ],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+      }
+    ])
+    .then((data) =>
+      createREADME(data));
+    //.catch((err) => console.log(err))
+};
+
 runCode();
 // Function call to initialize app();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,10 +10,57 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  
+  - ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions]{#questions}
+
+  ## Installation
+  
+  - ${data.installation}
+  
+  ## Usage
+  
+  - ${data.usage}
+  
+  ## Credits
+  
+  List your collaborators, if any, with links to their GitHub profiles.
+  
+  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+  
+  If you followed tutorials, include links to those here as well.
+  
+  ## License
+  
+  - ${data.license}
+  
+  ## How to Contribute
+  
+  - ${data.contribution}
+  
+  ## Tests
+  
+  - ${data.test}
+
+  ##Questions
+
+  - Need to ask a question? 
+  - Check out my github: [Git Hub Profile](https://github.com/${data.github})
+  - Send me an email: ${data.email}
 `;
 }
 
